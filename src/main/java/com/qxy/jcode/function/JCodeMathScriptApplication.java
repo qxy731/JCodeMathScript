7aa884368b105acaa97137a4f625146041b2b2f6
qxy731 <franke2009@163.com>
2018-08-16 10:26:04 +0800

    初始版本

---
+++ src/main/java/com/qxy/jcode/function/JCodeMathScriptApplication.java
@@ -0,0 +1,12 @@
+package com.qxy.jcode.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCodeMathScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JCodeMathScriptApplication.class, args);
+	}
+}
